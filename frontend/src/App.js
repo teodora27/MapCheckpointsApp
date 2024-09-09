eea2ec17a417ff41bcf4fc418147705a8d249ef9
teodora27 <cojocaruteodoralaura@yahoo.com>
2024-09-09 18:57:55 +0300

    added the map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
+import * as React from 'react';
+import { useState } from 'react';
+import Map from 'react-map-gl';
+import './App.css';
 
 
 function App() {
+  const [viewport, setViewport] = useState({
+    // width: '100%',
+    // height: '100%',
+    latitude: 47.040182,
+    longitude: 17.071727,
+    zoom: 4,
+  });
   return (
     <div className="App">
-      hello
+      <Map
+        {...viewport}
+        mapStyle="mapbox://styles/mapbox/streets-v11" 
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+        onMove={(evt) => setViewport(evt.viewState)}
+        //onViewportChange={nextViewport => setViewport(nextViewport)}
+      />
     </div>
   );
 }
