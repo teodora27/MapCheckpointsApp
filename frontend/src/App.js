e1afddb794ec8eed3c31d2594305e859e0f2214d
teodora27 <cojocaruteodoralaura@yahoo.com>
2024-09-10 17:22:05 +0300

    put the first pin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,43 @@
 import * as React from 'react';
 import { useState } from 'react';
 import Map from 'react-map-gl';
-import './App.css';
+import {Marker} from 'react-map-gl';
+import {Room} from "@material-ui/icons";
 
 
 function App() {
   const [viewport, setViewport] = useState({
-    // width: '100%',
-    // height: '100%',
-    latitude: 47.040182,
-    longitude: 17.071727,
+    latitude: 47,
+    longitude: 2,
     zoom: 4,
   });
   return (
-    <div className="App">
+    <div className="App"style={{ width: '100vw', height: '100vh'}}>
       <Map
         {...viewport}
         mapStyle="mapbox://styles/mapbox/streets-v11" 
+        style={
+          { 
+            width: '100vw', 
+            height: '100vh' 
+          }
+        }
         mapboxAccessToken={process.env.REACT_APP_MAPBOX}
         onMove={(evt) => setViewport(evt.viewState)}
         //onViewportChange={nextViewport => setViewport(nextViewport)}
-      />
+      
+      
+      >
+        
+        <Marker
+          latitude={47} 
+          longitude={2} 
+          //offset={[-3.5 * viewState.latitude,-7 * viewport.zoom]}
+          anchor="bottom"
+        >
+          <Room /> 
+        </Marker> 
+      </Map> 
     </div>
   );
 }
