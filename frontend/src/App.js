9aab0245b0ec9af5e1d3c9abbc33be2fd228aedd
teodora27 <cojocaruteodoralaura@yahoo.com>
2024-09-11 12:54:10 +0300

    conected the database to the app and showed the pins with popups

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,31 @@ import Map, {Popup} from 'react-map-gl';
 import {Marker} from 'react-map-gl';
 import {Room} from "@material-ui/icons";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [pins,setPins] = useState([]);
+
   const [viewport, setViewport] = useState({
     latitude: 47,
     longitude: 2,
     zoom: 4,
   });
+
+  React.useEffect(()=>{
+    const getPins = async()=>{
+      try {
+        const allPins = await axios.get("/pins");
+        setPins(allPins.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins()
+  },[])
+
   return (
-    <div className="App"style={{ width: '100vw', height: '100vh'}}>
+    <div className="App"style={{ width: '50vw', height: '100vh'}}>
       <Map
         {...viewport}
         mapStyle="mapbox://styles/mapbox/streets-v11" 
@@ -27,33 +43,39 @@ function App() {
         //onViewportChange={nextViewport => setViewport(nextViewport)}
       
       >
-        <Marker
-          latitude={47} 
-          longitude={2} 
+
+        {pins.map(p=>(
+          
+        <>  
+          <Marker
+          latitude={p.lat} 
+          longitude={p.long} 
           //offset={[-3.5 * viewState.latitude,-7 * viewport.zoom]}
           anchor="bottom"
         >
           <Room style={{fontSize:viewport.zoom*6, color: "rgb(222, 49, 99)"}}/> 
         </Marker> 
 
-        {/* <Popup 
-          latitude={47}
-          longitude={2} 
+        <Popup 
+          latitude={p.lat}
+          longitude={p.long} 
           offset={[2,-20]}
           anchor="left"
           // onClose={() => setShowPopup(false)}
           >
           <div className='card'>
           <label>Name</label>
-            <h4 className='name'>Center of France</h4>
+          <h4 className='name'>{p.name}</h4>
           <label>Description</label>
-            <p className='desc'>10/10 must visit as soon as possible</p>
+            <p className='desc'>{p.desc}</p>
           <label>Information: </label>
-          <span className='username'>Created by <b>Teo</b></span>
+          <span className='username'>Created by <b>{p.username}</b></span>
           <span className='date'> 1 hour ago</span>
           </div>
-      </Popup> */}
+      </Popup>
 
+        </>
+        ))}
 
       </Map> 
     </div>
