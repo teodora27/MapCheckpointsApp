c838c2ae37e46a7e8f33db762ff30d35e465c204
teodora27 <cojocaruteodoralaura@yahoo.com>
2024-09-12 21:31:24 +0300

    the popup interaction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,13 @@ import {Marker} from 'react-map-gl';
 import {Room} from "@material-ui/icons";
 import "./App.css";
 import axios from "axios";
+import {format} from "timeago.js";
 
 function App() {
-  const [pins,setPins] = useState([]);
+  const currentUser="ana_blandiana";
 
+  const [pins,setPins] = useState([]);
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
   const [viewport, setViewport] = useState({
     latitude: 47,
     longitude: 2,
@@ -25,7 +28,12 @@ function App() {
       }
     };
     getPins()
-  },[])
+  },[]);
+
+  const handleMarkerClick = (id) => {
+    setCurrentPlaceId(id);
+    // setViewport({ ...viewport, latitude: lat, longitude: long });
+  };
 
   return (
     <div className="App"style={{ width: '50vw', height: '100vh'}}>
@@ -46,35 +54,45 @@ function App() {
 
         {pins.map(p=>(
           
-        <>  
+        <React.Fragment key={p._id}>  
           <Marker
           latitude={p.lat} 
           longitude={p.long} 
           //offset={[-3.5 * viewState.latitude,-7 * viewport.zoom]}
           anchor="bottom"
-        >
-          <Room style={{fontSize:viewport.zoom*6, color: "rgb(222, 49, 99)"}}/> 
-        </Marker> 
+          >
+          <Room style={{fontSize:viewport.zoom*7, color: p.username===currentUser? "rgb(222, 49, 99)":"rgb(0,0,256)", cursor: "pointer"}}
+          onClick={() => handleMarkerClick(p._id)}
+          /> 
 
-        <Popup 
+
+        </Marker> 
+        {p._id === currentPlaceId ? (
+           
+          <Popup 
           latitude={p.lat}
           longitude={p.long} 
           offset={[2,-20]}
+          closeOnClick={false}  
+          closeOnMove={false}  
+          onClose={() => setCurrentPlaceId(null)}
           anchor="left"
-          // onClose={() => setShowPopup(false)}
+          
           >
-          <div className='card'>
-          <label>Name</label>
-          <h4 className='name'>{p.name}</h4>
-          <label>Description</label>
-            <p className='desc'>{p.desc}</p>
-          <label>Information: </label>
-          <span className='username'>Created by <b>{p.username}</b></span>
-          <span className='date'> 1 hour ago</span>
-          </div>
-      </Popup>
+            <div className='card'>
+            <label>Name</label>
+            <h4 className='name'>{p.name}</h4>
+            <label>Description</label>
+              <p className='desc'>{p.desc}</p>
+            <label>Information: </label> 
+            <span className='username'>Created by <b>{p.username}</b></span>
+            <span className='date'>{format(p.createdAt)}</span>
+            </div>
+        </Popup>
 
-        </>
+        ): null}
+
+        </React.Fragment>
         ))}
 
       </Map> 
